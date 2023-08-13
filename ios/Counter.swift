884047a53ca9bba6b54aaf52da6f903351100c9c
Monica Rivera <mriverap@outlook.com>
2023-08-13 00:54:57 -0700

    Fix Warning Module Counter requires main queue setup since it overrides 'init' but doesn't implement 'requiresMainQueueSetup

--- ios/Counter.swift
+++ ios/Counter.swift
@@ -16,4 +16,22 @@ class Counter: NSObject{
     count += 1;
     print(count);
   }
+
+@objc
+static func requiresMainQueueSetup() ->Bool{
+  /* 
+  True: RN to initialize the native module on the main thread before any 
+  JS code executes. This is needed when exporting constants from the module
+  and they are needed at initialization time or when the module creates UI
+  False: RN initializes the native module on the background thread
+  */
+
+  return true;
 }
+
+@objc
+func constantsToExport() -> [String: Any]!{
+  return ["initialCount": 0];
+}
+}
+
